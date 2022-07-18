0f8ca737de3483d4258c8b051e2118b32f280385
joel cronin <you@example.com>
2022-07-18 17:28:49 -0400

    about me page with NAV rendering and css linked

---
+++ src/components/AboutMe.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './aboutMe.css'
+import profilePic from '../assets/profilePic.jpg'
+
+
+
+export default function AboutMe() {
+
+  return (
+    <section className="container container-1">
+        <aside className="profile" id="profile-id">
+            <img src= {profilePic} alt="profile-pic" className="profPic"/> <br/><br/>
+            <h2>Joel Cronin</h2> <br/>
+            <p>University of Toronto</p><br/>
+            <p>Coding Bootcamp Student</p><br/>
+        </aside>
+        <section className="bio" id="bio-id">
+            <h2>About me</h2><br/>
+            <p>Hi, Im Joel Cronin, currently studying to become a Software Engineer with the University of Toronto's Coding Bootcamp. During this course I will learn the fundamentals of HTML, CSS and JavaScript languages. I also have acquired great experience in API design and am comfortable with JSON, AJAX and REST</p> <br/>
+            <p>Originally from London, UK I moved to Toronto in March 2022. In London I was a Mechanical and Electrical Engineer for 7 years with London Underground but have decided to transition into software development. </p>
+        </section>
+    </section>
+  );
+}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import './nav.css';
 
 export default function Nav() {
   const linkStyle = { border: '1px black', padding: '5px' };
 
   return (
-    <nav className="navigation">
-    <a href="#bio-id">About Me</a>
-    <a href="#project-id">Portfolio</a>
-    <a href="#contact-id">Contact</a>
-    <a href="#contact-id">Resume</a>
-</nav>
+    <div className="navigation">
+      <header class="header">
+        <h1>Joel Cronin</h1>
+      </header> 
+      <nav>
+        <a href="#bio-id">About Me</a>
+        <a href="#project-id">Portfolio</a>
+        <a href="#contact-id">Contact</a>
+        <a href="#contact-id">Resume</a>
+      </nav>
+    </div>
   );
 }
